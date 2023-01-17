66eed77b93c84fbb010cad730cc70bf4a9fda3ed
mesutfd <106558393+mesutfd@users.noreply.github.com>
2023-01-17 19:01:07 +0330

    initial commit

---
+++ product_module/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.product_list, name='product-list'),
+    path('', views.ProductListView.as_view(), name='product-list'),
+    path('cat/<cat>', views.ProductListView.as_view(), name='product-categories-list'),
+    path('test', views.test_preview, name='test'),
+    path('brand/<brand>', views.ProductListView.as_view(), name='product-brands-list'),
+    # path('<slug:slug>', views.product_detail, name='product-detail'),
+    path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('product-favorite', views.AddProductFavorite.as_view(), name='product-favorite'),
+]
